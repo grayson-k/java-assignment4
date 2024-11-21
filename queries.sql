50fcd9f7b235e51f72d41f98039353cb3a933c0f
Grayson Keaton <gkeaton23@gmail.com>
2024-11-21 18:11:11 -0500

    finished part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,14 @@
 --Part 1
+--/*
+--The job table has the following columns:
+--id - INT (Primary Key, Auto Increment)
+--employer - VARCHAR(255)
+--name - VARCHAR(255)
+--skills - VARCHAR(255)
+--*/
 
 --Part 2
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
